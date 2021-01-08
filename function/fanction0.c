0602e1ee671693f9b6c9fc1843477bbd762ad80e
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-01-08 21:38:21 +0900

    Add files via upload

---
+++ function/fanction0.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int ans(int a,int b);
+
+int main(void)
+{
+	int x,y,z;
+	
+	printf("”š‚ğ“ü—Í\n");
+	
+	scanf("%d",&x);
+	scanf("%d",&y);
+	
+	printf("%d\n",ans(x,y));
+	return 0;
+}
+
+int ans(int a,int b)
+{
+	int c;
+	
+	c=(a+b)/2;
+	
+	return c;
+}
