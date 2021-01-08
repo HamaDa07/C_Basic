824e6dfc3d337cec64d30afd52063def572c170f
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-01-08 19:55:41 +0900

    Add files via upload

---
+++ function/fanction1-1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int ans(int a){
+	int x;
+	x=a*a;
+	return(x);
+
+}
+	
+int main(void)
+{
+	
+	int b;
+	
+      printf("”š‚ğ“ü—Í\n");
+	scanf("%d",&b);
+	
+	printf("%d,%d",ans(b),b);
+	
+	return 0;
+}
+
+/*ŠÖ”–¼(•Ï”j‚ÅA•Ï”‚ğŠÖ”‚Éˆø‚«“n‚·*/
---
+++ function/fanction1-2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int ans(a,b)
+{
+	int x;
+	
+	x=(a+b)/2;
+	return (x);
+}
+
+int main(void)
+{
+	int c,d,e;
+	
+	printf("”š‚ğ“ü—Í\n");
+	scanf("%d",&c);
+	scanf("%d",&d);
+	
+		
+	printf("%d",ans(c,d));
+	
+	return 0;
+}
