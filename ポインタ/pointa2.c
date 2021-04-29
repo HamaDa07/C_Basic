ddd682cd5e2054fbf2934acde53ab4220d6b9f0a
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-04-29 18:35:40 +0900

    Update pointa2.c

--- ポインタ/pointa2.c
+++ ポインタ/pointa2.c
@@ -4,8 +4,8 @@ int main(void)
 	int *pt;
 	int mydt;
 	mydt=1234;
+	pt=&mydt;//代入
 	
-	pt=&mydt;
-	printf("mydt=%d\n",mydt);
-	printf("pt=%d\n",*pt);
+	printf("mydt=%d\n",mydt);//1234
+	printf("pt=%d\n",*pt);//1234
 }
